84dca5294564e6f2174bc027faf305256f3b8633
yerin <9yelin9@gmail.com>
2025-12-12 12:22:26 +0900

    Update

--- .hchain.omx/lib/.espresso_vqe/vqe.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pennylane as qml
-
-def vqe(npw, npwx, nvec, nvecx):
-	print("vqe.py:", npw, npwx, nvec, nvecx)
--- .hchain.omx/qsub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in `seq 0 1 19`
-do
-	qsub job/openmx-vqe-shot.sh $i
-done
